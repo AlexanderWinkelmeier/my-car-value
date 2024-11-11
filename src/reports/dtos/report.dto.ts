434ae2e2b4fe7153c0f109310f683976886fb01b
Alexander Winkelmeier <R.A.Winkelmeier@hotmail.de>
2024-11-11 08:23:12 +0100

    A Basic Permission System

--- src/reports/dtos/report.dto.ts
+++ src/reports/dtos/report.dto.ts
@@ -1,5 +1,5 @@
 import { Expose, Transform } from 'class-transformer';
-import { User } from 'src/users/user.entity';
+import { User } from '../../users/user.entity';
 
 export class ReportDto {
   @Expose()
@@ -7,17 +7,19 @@ export class ReportDto {
   @Expose()
   price: number;
   @Expose()
-  make: string;
-  @Expose()
-  model: string;
-  @Expose()
   year: number;
   @Expose()
   lng: number;
   @Expose()
   lat: number;
   @Expose()
+  make: string;
+  @Expose()
+  model: string;
+  @Expose()
   mileage: number;
+  @Expose()
+  approved: boolean;
 
   @Transform(({ obj }) => obj.user.id)
   @Expose()
